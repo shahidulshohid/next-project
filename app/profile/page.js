ecf579f8b3da8c0c996e0b1cd37c109aeb76168e
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-24 02:52:48 +0600

    redirect to login page is done

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,10 +1,23 @@
-
-const Profile = () => {
-    return (
-        <main className="mt-10">
-            <h2 className="text-3xl font-bold text-center mt-20 mb-5">Welcome to your Profile</h2>
-        </main>
-    );
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+const Profile = async () => {
+  const { getUser, isAuthenticated} = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+  return (
+    <main className="mt-10">
+      <h2 className="text-3xl font-bold text-center mt-20 mb-5">
+        Welcome to your Profile
+      </h2>
+      {(await isAuthenticated()) ? (
+        <div className="text-center">
+          <h2 className="text-xl font-semibold my-3">{user.given_name} {user.family_name}</h2>
+          <p className="font-semibold">{user.email}</p>
+        </div>
+      ) : (
+        <p>Please sign in or register!</p>
+      )}
+    </main>
+  );
 };
 
 export default Profile;
