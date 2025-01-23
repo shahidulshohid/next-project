727c3641558535a3f3767fc9761e9bb493537d20
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-23 21:55:19 +0600

    protected page start now

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -7,7 +7,7 @@ const BlogDetails = async ({ params }) => {
   return (
     <div>
       <h2 className="text-2xl font-bold text-center mt-20 mb-5">Blog Details</h2>
-      <div className="w-1/2 mx-auto bg-gray-200 px-5 py-6">
+      <div className="md:w-1/2 mx-auto bg-gray-200 px-5 py-6">
         <div className="text-center border">
           <h2 className="text-xl font-semibold mb-2">{data.title}</h2>
           <p className="text-sm font-semibold">{data.body}</p>
